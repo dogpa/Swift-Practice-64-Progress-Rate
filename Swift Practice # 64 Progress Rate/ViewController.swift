cedab0a2c278919e2af038a4422505e090c6dcd8
Dogpa <dogpa0130@gmail.com>
2021-08-22 13:21:54 +0800

    Initial Commit

---
+++ Swift Practice # 64 Progress Rate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 64 Progress Rate
+//
+//  Created by Dogpa's MBAir M1 on 2021/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
